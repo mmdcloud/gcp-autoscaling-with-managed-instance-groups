fb545fa4adad3bf95da47f5e80252e7212065e12
mmdcloud <mmd26.aka@gmail.com>
2024-08-11 14:20:45 +0000

    update: added user_data script

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,27 +76,24 @@ resource "google_compute_instance_template" "nodeapp-template" {
     boot         = true
   }
 
-  # install nginx and serve a simple web page
+  # install nginx and nodejs with application
   metadata = {
     startup-script = <<-EOF1
-      #! /bin/bash
-      set -euo pipefail
-
-      export DEBIAN_FRONTEND=noninteractive
-      apt-get update
-      apt-get install -y nginx-light jq
-
-      NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
-      IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
-      METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
-
-      cat <<EOF > /var/www/html/index.html
-      <pre>
-      Name: $NAME
-      IP: $IP
-      Metadata: $METADATA
-      </pre>
-      EOF
+        #!/bin/bash
+	sudo apt-get update
+	sudo apt-get install -y nginx
+	sudo apt update
+	curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+	sudo bash nodesource_setup.sh
+	sudo apt install nodejs -y
+	cd /home/surajmishratheboss
+	git clone https://github.com/mmdcloud/gcp-autoscaling-with-managed-instance-groups
+	cd aws-autoscaling-with-load-balancing
+	sudo cp scripts/nodejs_nginx.config /etc/nginx/sites-available/default
+	sudo service nginx restart
+	sudo npm i
+	sudo npm i -g pm2
+	pm2 start server.mjs
     EOF1
   }
   lifecycle {
